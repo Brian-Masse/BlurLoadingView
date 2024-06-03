7b76e9ab6739462987c2b4b1573db42e648c2f89
Brian Masse <brianm25it@gmail.com>
2024-06-02 18:59:28 -0700

    initial commit

---
+++ LoadingBlurDemo/LoadingBlurDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingBlurDemoApp.swift
+//  LoadingBlurDemo
+//
+//  Created by Brian Masse on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct LoadingBlurDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
